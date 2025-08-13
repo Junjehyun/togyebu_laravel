11aabaf99d3e57279eb2d39dc2be70f9fd860269
jehyun.jun <h94051987@gmail.com>
2025-08-13 15:29:27 +0900

    Controller 구성

--- togyebu_laravel/routes/web.php
+++ togyebu_laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', MainController::class, 'index')->name('index');
